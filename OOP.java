017e2efcffbe2fd95e42287dcfc70ef01d7e7794
Bilgehan611 <bilgehansaglamturk@gmail.com>
2026-01-24 18:00:31 +0300

    OOP.java

--- OOP.java
+++ OOP.java
@@ -1,23 +1,30 @@
 public class OOP {
+
     String Ogretmen;
     String Odev;
+    String Gun;
     int YazılıNotu;
     int SozluNotu;
 
-    public OOP(String Ogretmen, String Odev ,
+    public OOP(String Ogretmen, String Odev, String Gun,
                int YazılıNotu, int SozluNotu) {
+
         this.Ogretmen = Ogretmen;
-                this.Odev = Odev;
-                        this.YazılıNotu = YazılıNotu;
-                                this.SozluNotu = SozluNotu;
+        this.Odev = Odev;
+        this.Gun = Gun;
+        this.YazılıNotu = YazılıNotu;
+        this.SozluNotu = SozluNotu;
     }
-public void showInfo() {
-    System.out.println("Öğretmen: " + Ogretmen);
-    System.out.println("Ödev: " + Odev);
-    System.out.println("Yazılı Notu: " + SozluNotu);
-    System.out.println("Sözlü Notu: " + YazılıNotu);
-}
+
+    public void showInfo() {
+        System.out.println("Öğretmen: " + Ogretmen);
+        System.out.println("Ödev: " + Odev);
+        System.out.println("Gün: " + Gun);
+        System.out.println("Yazılı Notu: " + YazılıNotu);
+        System.out.println("Sözlü Notu: " + SozluNotu);
+    }
+
     public double getAverage() {
-        return (SozluNotu + YazılıNotu)/2;
+        return (SozluNotu + YazılıNotu) / 2.0;
     }
 }
