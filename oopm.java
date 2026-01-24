2f44520146be2e372fd6e1b7f31d977cd3c7b79c
Bilgehan611 <bilgehansaglamturk@gmail.com>
2026-01-24 18:01:04 +0300

    OOPMAİN.java
    Refactor main method to use an array of OOP objects and display their information.

--- oopm.java
+++ oopm.java
@@ -2,15 +2,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        OOP mat = new OOP(
-                "Murro",
-                "Matematik 2. ünite sonu",
-                68,
-                94
-        );
+        OOP[] dersler = new OOP[5];
 
-        mat.showInfo();
+        dersler[0] = new OOP("Murro Hoca", "Matematik ödevi", "Pazartesi", 80, 90);
+        dersler[1] = new OOP("Fatmagül Hoca", "Türkçe kompozisyon", "Salı", 75, 85);
+        dersler[2] = new OOP("Derya Hoca", "Fen deney raporu", "Çarşamba", 60, 70);
+        dersler[3] = new OOP("Yeliz Hoca", "İngilizce kelime ezberi", "Perşembe", 90, 95);
+        dersler[4] = new OOP("Hoca Hoca", "Tarih sunumu", "Cuma", 85, 80);
 
-        System.out.println("Ortalama: " + mat.getAverage());
+        for (int i = 0; i < dersler.length; i++) {
+
+            System.out.println("------- " + (i + 1) + ". Gün -------");
+            dersler[i].showInfo();
+            System.out.println("Ortalama: " + dersler[i].getAverage());
+            System.out.println();
+        }
     }
 }
